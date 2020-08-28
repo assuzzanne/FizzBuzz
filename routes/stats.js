7e108f9d5ff201a4f2060127925dc627c7c65074
Aude Rouaux <auderouaux@Audes-MacBook-Pro.local>
2020-08-28 17:18:47 +0200

    feat(stats): implement route for stats

--- routes/stats.js
+++ routes/stats.js
@@ -3,31 +3,14 @@ var router = express.Router();
 
 // const statsHandler = require("../handlers/stats");
 
-/* Get params and call computing function from handler */
-router.get("/int/:int1/:int2/:limit/str/:str1/:str2/", function (
+/*  */
+router.get("/", function (
   req,
   res,
   next
 ) {
-  // Accepts five parameters : three integers int1, int2 and limit, and two strings str1 and str2.
-  // to optimise
-
-  // const result = customisedBuzzFeedHandler(
-  //   parseInt(req.params.int1),
-  //   parseInt(req.params.int2),
-  //   parseInt(req.params.limit),
-  //   req.params.str1,
-  //   req.params.str2
-  // );
-  // res.send(result);
-
-  res.send(customisedBuzzFeedHandler(
-    parseInt(req.params.int1),
-    parseInt(req.params.int2),
-    parseInt(req.params.limit),
-    req.params.str1,
-    req.params.str2
-  ));
+    // Return the parameters corresponding to the most used request, as well as the number of hits for this request
+    res.send("coucou");
 });
 
 module.exports = router;
