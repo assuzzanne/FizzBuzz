5c895c346d05b15b2936bce9693094e01167711e
Aude Rouaux <auderouaux@Audes-MacBook-Pro.local>
2020-08-31 00:35:15 +0200

    feat(chore): return the most used request

--- routes/stats.js
+++ routes/stats.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+const Request = require("../models/request");
+
 // const statsHandler = require("../handlers/stats");
 
 /*  */
-router.get("/", function (
+router.get("/", async function (
   req,
   res,
   next
 ) {
     // Return the parameters corresponding to the most used request, as well as the number of hits for this request
-    res.send("coucou");
+    const mostUsedRequest =  await Request.find().sort({counter : -1}).limit(1);
+    res.send(mostUsedRequest);
 });
 
 module.exports = router;
