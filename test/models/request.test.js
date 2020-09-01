83d2b42cfa6478c1b65f7de13318955d52188ad0
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-01 17:24:42 +0200

    chore(model): unit test Request mongoose schema

---
+++ test/models/request.test.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+const Request = require("../../models/request");
+
+chai.use(chaiAsPromised);
+
+describe("Request Schema", () => {
+  it("should fail if required fields are missing", async () => {
+    const request = await new Request();
+    chai.expect(request.validate()).to.eventually.be.rejected;
+  });
+});
