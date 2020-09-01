7228c7e0e758f0d76e72c909076a78fa6524c087
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-01 17:25:18 +0200

    chore(test): refacto imports of customised FizzBuzz route test

--- test/routes/customisedFizzBuzz.test.js
+++ test/routes/customisedFizzBuzz.test.js
@@ -1,11 +1,8 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const should = chai.should();
 
 const app = require("../../app");
-
 const Request = require("../../models/request");
-const { assert } = require("chai");
 
 chai.use(chaiHttp);
 
@@ -42,8 +39,8 @@ describe("customisedFizzBuzz", () => {
         )
         .send();
 
-      assert.equal(res.status, 200);
-      assert.deepEqual(
+      chai.assert.equal(res.status, 200);
+      chai.assert.deepEqual(
         res.text,
         "1,2,pomodoro,4,rosso,pomodoro,7,8,pomodoro,rosso,11,pomodoro,13,14,pomodororosso,16,17,pomodoro,19,rosso,pomodoro,22,23,pomodoro,rosso,26,pomodoro,28,29,pomodororosso,31,32,pomodoro,34,rosso,pomodoro,37,38,pomodoro,rosso,41,pomodoro,43,44,pomodororosso,46,47,pomodoro,49"
       );
