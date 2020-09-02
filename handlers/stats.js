2c93200ee6793804e404402cfc2bce94ba4d6cbe
Aude Rouaux <aude.s.rouaux@gmail.com>
2020-09-02 13:22:07 +0200

    feat(stats): handle if db query find nothing

--- handlers/stats.js
+++ handlers/stats.js
@@ -2,7 +2,10 @@ const Request = require("../models/request");
 
 module.exports = async function format() {
   const mostUsedRequest = await Request.find().sort({ counter: -1 }).limit(1);
+  if (!mostUsedRequest.length) {
+    return "Failed to find request!";
+  }
+
   const [{ params, counter }] = mostUsedRequest;
-  
   return `The parameters corresponding to the most used request are ${params.int1}, ${params.int2}, ${params.limit}, ${params.str1}, ${params.str2}. The number of hits for this request is ${counter}.`;
 };
